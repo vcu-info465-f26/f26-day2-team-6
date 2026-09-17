0c7a128f092f85c8123a8d519126a263e58e966f
alanamac222 <alanamaceachron@gmail.com>
2026-09-17 20:49:23 +0000

    fix fetch_currency_names.py, wire up build_db.py to correct imports

--- src/fetch_currency_names.py
+++ src/fetch_currency_names.py
@@ -1,8 +1,6 @@
 import requests
-import pandas as pd
 
 def get_currency_names():
-
     currency_names_url = "https://api.frankfurter.dev/v2/currencies"
     # fetch the list of currencies (the names of the currencies & their symbols)
     currency_names_response = requests.get(currency_names_url)
@@ -11,16 +9,6 @@ def get_currency_names():
     currency_names_data = currency_names_response.json()
     return currency_names_data
 
-
-# The Block Below Puts the Response in a Table To Print
-
-currency_names_rows = get_currency_names()
-# stores the list of currency data dictionaries (Messy Looking Data)
-
-currency_names_df = pd.DataFrame(currency_names_rows)
-# Turns the list of currency data dictionaries from the API response into a dataframe/table
-
-currency_names_df = currency_names_df.drop(columns=["iso_numeric", "start_date", "end_date"])
-# Drops the numeric codes & useless dates (keeps code, name, and symbol EX: USD, United States Dollar, $)
-
-print(currency_names_df)
+if __name__ == "__main__":
+    rows = get_currency_names()
+    print(rows[0])
--- src/fetch_rates.py
+++ src/fetch_rates.py
@@ -4,7 +4,7 @@ def get_latest_rates(base="USD", quotes="EUR,GBP,JPY"):
     url = "https://api.frankfurter.dev/v2/rates"
 
     # send the request
-    response = requests.get(url, params={"base": base, "quotes": quotes})
+    response = requests.get(url, params={"base": base, "symbols": quotes})
 
     # turn the response into a python list of rows
     data = response.json()
